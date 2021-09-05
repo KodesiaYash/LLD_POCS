6f39e28762900acb9e68b0bbabf77190227e05e5
Yash Kodesia <kodesiay@amazon.com>
2021-09-05 07:32:15 +0530

    Setup project

---
+++ src/main/java/com/example/lldesign/pocs/PocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lldesign.pocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocsApplication.class, args);
+    }
+
+}
