4c9fb3c65c5393e38521bcf026389aa312a775df
Yash Kodesia <kodesiay@amazon.com>
2021-09-05 07:32:15 +0530

    Setup project

---
+++ src/test/java/com/example/lldesign/pocs/PocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lldesign.pocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
